9a1e292721305477ef35d9499b70937c62203ea1
kenzie324 <Kenzie.gantheng@gmail.com>
2025-10-03 14:22:44 +0700

    belajar collection

---
+++ 22_collection.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  /**
+   *  list => berurutan , dibaca dari index 0
+   *  Sel => Acak
+   *  Map => key value, dibaca dari key
+   * 
+   *  collection adalah struktur data yang digunakan untuk 
+   * menyimpan banyak nilai sekaligus
+   */
+}
