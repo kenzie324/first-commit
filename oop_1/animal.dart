00129beba9b74ee35c817c31eefabba1da496177
kenzie324 <Kenzie.gantheng@gmail.com>
2025-10-07 14:23:01 +0700

    belajar oop animal

---
+++ oop_1/animal.dart
@@ -0,0 +1,20 @@
+class Animal {
+  // atribute (properties)
+  String name = 'Mbul';
+  String colorSkin = 'Black n White';
+  double weight = 2;
+  int age = 2;
+
+
+
+  // metod / functon /behaivor (perilaku)
+  void eat() {
+    print('$name eating chiken ${weight + 1} kg');
+  }
+  void poop() {
+    print('$name still poop in the toilet');
+  }
+  void sleep() {
+    print('$name still sleep');
+  }
+}
