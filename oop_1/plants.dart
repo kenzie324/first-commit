9a7fbf3b2293d8b0732c4eb7108bbb42df8a17bd
kenzie324 <Kenzie.gantheng@gmail.com>
2025-10-07 14:23:40 +0700

    plants oop

---
+++ oop_1/plants.dart
@@ -0,0 +1,9 @@
+class Plants {
+  String? name;
+  String? latinName;
+  String? color;
+  double? height;
+  int? price;
+
+  Plants(this.name, this.latinName, this.color, this.height, this.price);
+}
