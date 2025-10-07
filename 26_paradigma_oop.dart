c344d9d4c517a9bb2823504cac82f84b3f6895a7
kenzie324 <Kenzie.gantheng@gmail.com>
2025-10-07 14:22:47 +0700

    belajar paradigma

--- 26_paradigma_oop.dart
+++ 26_paradigma_oop.dart
@@ -24,4 +24,33 @@ class Manusia {
   void berjalan() {}
   void belajar() {}
 
+// 2.abstraction
+/**Absraksi juga bisa dibilang merupakan penerapan alami
+ * dari encipsulation.
+ * Absraksi berati sebuah objek hanya menunjukan operanya
+ * secara high-level
+ * 
+ * misal kita cukup tahu bagaimana ucup makan namun
+ * kita tidak perlu tahu seperti apa metabolisme biologis
+ * dalam tubuh ucup yang membuat berat badannya
+ * bertambah
+ *
+ * 
+ * 3. inheritance
+ * inheritance beberapa objek bisa memiliki beberapa karakteristik 
+ * atau perilaku yang sama
+ * 
+ * namun, bukanlah objek yang sama
+ * 
+ * 4. Polymorphism
+ * dalam bahasa yunani berati 'banyak bentuk'
+ * sederhana objek dapat memiliki bentuk 
+ * atau impelementasi yang berbeda-beda
+ * pada suatu metod yang sama.
+ * 
+ * Semua manusia bergerak , namun tentu gerak ucup
+ * dan vladimir memiliki cara gerak yang berbeda
+ * perbedaaan bentuk atau cara gerak tersebut merupakan 
+ * contoh dari Polymorphism
+ */
 }
